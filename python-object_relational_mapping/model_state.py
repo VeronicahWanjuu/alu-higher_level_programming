2bf0cbecb5fea959dc15f4c2019a2b6da2b0805d
VeronicahWanjuu <v.wanjuu@alustudent.com>
2024-12-01 07:48:36 -0800

    Fix PEP 8 issues in model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """
     State class that links to the states table in MySQL database.
@@ -14,4 +15,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-
